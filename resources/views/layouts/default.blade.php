ad4b411af0ada3d10d54871112ede8f4e1b77a89
Guilher0 <guilherme.bs10@gmail.com>
2023-07-01 20:53:21 -0300

    Alterei Titles e BG

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,12 +5,15 @@
     <title>Magia Divina</title>
     <link rel="stylesheet" href="{{asset('css/index.css')}}"/>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
         <nav style="position: absolute;">
             <div style="margin:15px 0 0 10px; ">
-                <a href="{{''}}"><img src="{{'img-index/logo-nav.png'}}" alt="logo" style="max-width: 150px"></a>
+                <a href="{{''}}"><img src="{{'imgIndex/logo-nav.png'}}" alt="logo" style="max-width: 250px"></a>
             </div>
         </nav>
     </header>
--- resources/views/layouts/pageone.blade.php
+++ resources/views/layouts/pageone.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <section>
     {{--Cria a imagem de fundo com o titulo da pagina--}}
-    <div id="img-bg" class="bg bg-dark bg-gradient"></div>
+    <div id="img-bg" class="bg img-bg" ></div>
     <div id="title" class="title-img">
         <h1 class="">
             A Magia no equilíbrio e cura do ser humano e animais.
